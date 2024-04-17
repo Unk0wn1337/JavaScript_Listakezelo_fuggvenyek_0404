dda87c4334c08a5437e6f26c64e47d4e2103f81c
Unk0wn1337 <kelogames@gmail.com>
2024-04-17 13:50:06 +0200

    feladatok elvegezese

---
+++ adatKezelo.js
@@ -0,0 +1,41 @@
+export function tablazatRendez(lista,irany){
+    /*adott mező (név) - adott mezőszerint rendezi a táblázatot. - akkor hívódik meg, ha a tablazat
+    nev fejlecere kattintunk. Rendezzük a listát, megjelenitjuk újra a táblázatot.
+   kattintunk */ 
+    lista.sort(function(a,b){
+        /*Szorgalmi: a.nev első betűjének karakterkódja alapján (ASCI)*/
+        return a.nev.toUpperCase() > b.nev.toUpperCase() ? 1*irany : -1*irany;
+        /*pozitiv vagy negativ kell */
+    });
+    return lista;
+
+}
+/*Szorgalmi HF: lehessen rendezni kor szerint is!* *** tetszőleges mező szerint/ */
+
+
+
+function atalakit(szoveg){
+    // szorgalmi házi feladat, hogy az Á betűt kicseréljük A-ra az É - E-re
+    // replaceAll függvény btw
+}
+
+export function szuresNevSzerint(lista, szurtSzoveg){
+    /*szuresNevSzerint(lista, szurtSzoveg) - a szűrőben írt szó alapján kilistázza azokat az adatokat a listából, amelyekben szerepel a név mezejében az adott szó 
+   Ezután megjelenítjük a szürt listát az oldalon.
+   Akkor fog lefutni amikor megváltozik a szűrőmező tartalma.*/
+   const SZURTLISTA = lista.filter(function(elem){
+        return elem.nev.includes(szurtSzoveg)
+   });
+   console.log(SZURTLISTA);
+   return SZURTLISTA;
+
+}
+
+export function sorTorles(lista,index){
+    
+    /*sorTorles(lista, id) - minden sor végén legyen egy kuka a sor indexével, erre a kukára kattintva töröljük az 
+   adott sort listából, és újra megjelenítjük a táblázatot a módosult listával. */
+    lista.splice(index,-1);
+    return lista;
+
+}
